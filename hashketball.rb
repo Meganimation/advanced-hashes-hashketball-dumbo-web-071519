d2c78bcfba011cab896252c44f2a49df5819fc28
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-11 23:32:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,31 @@
 # Write your code here!
 
 
+def game_hash
+  
+  game = {
+    :home => {:team_name => "Brooklyn Nets", :colors => ["Black", "Net"], :players => 
+    
+    [
+      {:player_name => {}},
+      {:number => {}},
+      {:shoe => {}}, 
+      {:rebounds => {}},
+      {:assists => {}},
+      {:steals => {}},
+      {:blocks => {}},
+      {:slam_dunks => {}}
+      
+      ]},
+    :away => {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"], :players => []}
+  }
+  
+end
+
+
+def num_points_scored 
+  
+end 
 
 
 
