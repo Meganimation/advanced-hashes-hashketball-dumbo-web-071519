5e4d0538dbd44e600044ea8480d051dda2f3f898
Megan Adams <meganoliviawendyadams@gmail.com>
2019-07-11 23:35:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,10 @@
-# Write your code here!
 
-
-# Write your code here!
 require 'pry'
 
 def game_hash
   {
     away: { team_name: 'Charlotte Hornets',
-            colors: %w[Turquoise Purple],
+            colors: ["Turquoise", "Purple"],
             players: [
               { player_name: 'Jeff Adrien',
                 number: 4,
@@ -56,7 +53,7 @@ def game_hash
                 slam_dunks: 12 }
             ] },
     home: { team_name: 'Brooklyn Nets',
-            colors: %w[Black White],
+            colors: ["Black", "White"],
             players: [
               { player_name: 'Alan Anderson',
                 number: 0,
@@ -114,9 +111,9 @@ def num_points_scored(sought_player_name)
 
       data.each do |player|
         return player[:points] if player[:player_name] == sought_player_name
-      end
-    end
-  end
+end
+end
+end
 end
 
 def shoe_size(sought_player_name)
@@ -233,9 +230,7 @@ def most_points_scored
 end
 
 def winning_team
-  # Set up a hash to keep track of the points scored by each team. This way, we
-  # can iterate through each player, get their points scored, and increase the
-  # count in the hash.
+  
 
   scores = { 'Brooklyn Nets' => 0, 'Charlotte Hornets' => 0 }
 
